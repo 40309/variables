eeee9ea4c8ec28129ece4ea67a293a4ad05dbfe5
Tony Karimpumannil <40309@longroad.ca.uk>
2014-09-09 15:08:39 +0100

    Exercise 1.2

---
+++ Exercise 1.2.py
@@ -0,0 +1,13 @@
+#Tony Karimpumannil
+#09-09-2014
+#Exercise 1.2 - Variables
+
+print("Hello World")
+#This prints out "Hello World"
+first_name = input("Tony")
+# first_name is a variable
+# (=) means assign
+print(first_name)
+# This prints out the variable first_name
+print("Hey {0}!" .format(first_name))
+#This is the final command, 
