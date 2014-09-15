ccea1f9328bc7a7be1f8c410f7ac4148b5e4b65f
Tony Karimpumannil <40309@longroad.ca.uk>
2014-09-15 12:18:06 +0100

    Development exercise

---
+++ Development Exercise 1.py
@@ -0,0 +1,16 @@
+#Tony K.
+#15/09/2014
+#Development Exercise 1
+
+number1 = int(input("Please enter the number ou are going to divide: "))
+number2 = int(input("Please enter the number you are going to divide it by: "))
+
+remainder = number1//number2
+
+answer = number1/number2
+
+print(" {1} a remainder of {1}
+
+
+
+
